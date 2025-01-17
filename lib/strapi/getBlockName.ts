fd74a51f7d814c5b2510d6535c1c08d992fb2568
Albin Gerhardsson <albin.gerhardsson@gracestudio.se>
2025-01-17 08:09:36 +0100

    Change from blockname to typename in getBlockName function

--- lib/strapi/getBlockName.ts
+++ lib/strapi/getBlockName.ts
@@ -3,5 +3,5 @@ const clearAndUpper = (text: string) => text.replace(/-/, '').toUpperCase();
 const toPascalCase = (text: string) =>
   text.replace(/(^\w|-\w)/g, clearAndUpper);
 
-export const getBlockName = <T>(block: { __component: string }) =>
-  toPascalCase(block.__component.replace('blocks.', '')) as keyof T;
+export const getBlockName = <T>(block: { __typename: string }) =>
+  toPascalCase(block.__typename.replace('ComponentBlocks', '')) as keyof T;
