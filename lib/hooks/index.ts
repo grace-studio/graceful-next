26fe3f682af4aac4d2e6ebff31fee76b5ccd393b
Mattias Alm <mattias.alm@gracestudio.se>
2025-11-25 14:33:08 +0100

    remove tests

--- lib/hooks/index.ts
+++ lib/hooks/index.ts
@@ -4,6 +4,3 @@ export * from './usePreventBodyScroll';
 export * from './useOutsideClick';
 export * from './useGoogleTagManager';
 export * from './useSetClassOnTab';
-
-// External
-export * from 'react-hotkeys-hook';
